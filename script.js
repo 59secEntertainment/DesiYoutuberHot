2fc3d73fa3dd9b99b8cfb27c29e0f47a2d3c3396
59secEntertainment <59secentertainment@gmail.com>
2025-01-25 21:38:34 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,18 @@
-document.addEventListener("DOMContentLoaded", loadImages);
+let db;
+
+document.addEventListener("DOMContentLoaded", () => {
+    let request = indexedDB.open("PhotoDB", 1);
+
+    request.onupgradeneeded = function(event) {
+        let db = event.target.result;
+        db.createObjectStore("photos", { keyPath: "id", autoIncrement: true });
+    };
+
+    request.onsuccess = function(event) {
+        db = event.target.result;
+        loadImages();
+    };
+});
 
 function uploadImage() {
     const fileInput = document.getElementById("imageUpload");
@@ -14,38 +28,47 @@ function uploadImage() {
     reader.onload = function(event) {
         const imageData = event.target.result;
         saveImage(title, description, imageData);
-        displayImage(title, description, imageData);
     };
     reader.readAsDataURL(fileInput.files[0]);
 }
 
 function saveImage(title, description, imageData) {
-    let images = JSON.parse(localStorage.getItem("images")) || [];
-    images.push({ title, description, imageData });
-    localStorage.setItem("images", JSON.stringify(images));
+    let transaction = db.transaction(["photos"], "readwrite");
+    let store = transaction.objectStore("photos");
+    store.add({ title, description, imageData });
+
+    transaction.oncomplete = function() {
+        alert("Image uploaded successfully!");
+        loadImages();
+    };
 }
 
 function loadImages() {
-    let images = JSON.parse(localStorage.getItem("images")) || [];
-    images.forEach((img, index) => displayImage(img.title, img.description, img.imageData, index));
+    let transaction = db.transaction(["photos"], "readonly");
+    let store = transaction.objectStore("photos");
+    let request = store.getAll();
+
+    request.onsuccess = function() {
+        const gallery = document.getElementById("gallery");
+        gallery.innerHTML = "";
+        request.result.forEach(photo => displayImage(photo));
+    };
 }
 
-function displayImage(title, description, imageData) {
+function displayImage(photo) {
     const gallery = document.getElementById("gallery");
 
     const div = document.createElement("div");
     div.className = "image-card";
     div.innerHTML = `
-        <img src="${imageData}" alt="${title}">
-        <h3>${title}</h3>
+        <img src="${photo.imageData}" alt="${photo.title}">
+        <h3>${photo.title}</h3>
     `;
 
     div.onclick = function() {
-        // Store image data in localStorage before opening photo.html
-        localStorage.setItem("selectedPhoto", JSON.stringify({ title, description, imageData }));
+        localStorage.setItem("selectedPhotoId", photo.id);
         window.location.href = "photo.html";
     };
 
     gallery.appendChild(div);
 }
-
